b01d00d85b0d349925adb3925fc384872587af18
Katarzyna Fronc <katarzynaanna.fronc@gmail.com>
2022-11-21 17:31:29 +0100

    Array and objects students were created

---
+++ Zadanie11/script.js
@@ -0,0 +1,7 @@
+const student1 = { id: 123, name: "Jan", lastName: "Kowalski", personalIdentityNumber: 111222333, fieldOfStudy: "Informatics", notes: [3, 4, 2, 4, 3, 4] };
+const student2 = { id: 462, name: "Anna", lastName: "Stardust", personalIdentityNumber: 222333444, fieldOfStudy: "Astronomy", notes: [4, 4, 5, 3, 4, 5] };
+const student3 = { id: 034, name: "Tom", lastName: "Major", personalIdentityNumber: 111222333, fieldOfStudy: "Astronautics", notes: [3, 4, 2, 4, 3, 4] };
+const student4 = { id: 264, name: "Kate", lastName: "Moon", personalIdentityNumber: 555666777, fieldOfStudy: "Architecture", notes: [5, 4, 3, 4, 5, 4] };
+const student5 = { id: 326, name: "Adam", lastName: "Nowak", personalIdentityNumber: 333444555, fieldOfStudy: "politicalScience", notes: [3, 2, 2, 4, 5, 4] };
+const students = [student1, student2, student3, student4, student5];
+console.log(students);
