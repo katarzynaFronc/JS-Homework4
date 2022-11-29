c130948e715c93b770f1be78a47e3f83f7ffe32b
Katarzyna Fronc <katarzynaanna.fronc@gmail.com>
2022-11-29 20:13:17 +0100

    the code has been simplified

--- Zadanie11/script.js
+++ Zadanie11/script.js
@@ -1,14 +1,19 @@
 // Exercise 1
-const student1 = { id: 12, name: "Jan", lastName: "Kowalski", personalIdentityNumber: 111222333, fieldOfStudy: "Informatics", notes: [3, 4, 2, 4, 3, 4] };
-const student2 = { id: 46, name: "Anna", lastName: "Stardust", personalIdentityNumber: 222333444, fieldOfStudy: "Astronomy", notes: [4, 4, 5, 3, 4, 5] };
-const student3 = { id: 34, name: "Tom", lastName: "Major", personalIdentityNumber: 111222333, fieldOfStudy: "Astronautics", notes: [3, 4, 2, 4, 3, 4] };
-const student4 = { id: 26, name: "Kate", lastName: "Moon", personalIdentityNumber: 555666777, fieldOfStudy: "Architecture", notes: [5, 4, 3, 4, 5, 4] };
-const student5 = { id: 32, name: "Adam", lastName: "Nowak", personalIdentityNumber: 333444555, fieldOfStudy: "PoliticalScience", notes: [3, 2, 2, 4, 5, 4] };
-let students = [student1, student2, student3, student4, student5];
+// Array with students data
+const students = [
+  { id: 12, name: "Jan", lastName: "Kowalski", pesel: 111222333, fieldOfStudy: "Informatics", notes: [3, 4, 2, 4, 3, 4] },
+  { id: 46, name: "Anna", lastName: "Stardust", pesel: 222333444, fieldOfStudy: "Astronomy", notes: [4, 4, 5, 3, 4, 5] },
+  { id: 34, name: "Tom", lastName: "Major", pesel: 111222333, fieldOfStudy: "Astronautics", notes: [3, 4, 2, 4, 3, 4] },
+  { id: 26, name: "Kate", lastName: "Moon", pesel: 555666777, fieldOfStudy: "Architecture", notes: [5, 4, 3, 4, 5, 4] },
+  { id: 32, name: "Adam", lastName: "Nowak", pesel: 333444555, fieldOfStudy: "PoliticalScience", notes: [3, 2, 2, 4, 5, 4] },
+];
+
 console.log(students);
 
-//Exercise 2
-function addNewstudent(name, lastName, personalIdentityNumber, fieldOfStudy, notes) {
+// Exercise 2
+// function addNewStudent
+
+function addNewstudent(name, lastName, pesel, fieldOfStudy, notes) {
   function getRandomId(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -19,7 +24,7 @@ function addNewstudent(name, lastName, personalIdentityNumber, fieldOfStudy, not
     id: id,
     name: name,
     lastName: lastName,
-    personalIdentityNumber: personalIdentityNumber,
+    pesel: pesel,
     fieldOfStudy: fieldOfStudy,
     notes: notes,
   };
@@ -30,8 +35,8 @@ function addNewstudent(name, lastName, personalIdentityNumber, fieldOfStudy, not
 addNewstudent("Ilona", "Brain", 777888999, "Alienology", [2, 5, 2, 5, 2, 5]);
 
 // Exercise 3
-function studentsList(students) {
-  return students.map((student) => `Student ${student.id}: ${student.name} ${student.lastName}, PESEL: ${student.personalIdentityNumber}, Fields of study: ${student.fieldOfStudy}`);
-}
+// function that displays a list of students
 
-console.log(studentsList(students));
+students.map(function (student) {
+  console.log(`Student ${student.id}: ${student.name} ${student.lastName}, PESEL: ${student.pesel}, Fields of study: ${student.fieldOfStudy}`);
+});
