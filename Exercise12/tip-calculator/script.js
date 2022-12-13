9a337893f902ade1d7978b47053cb49bd4bb22a7
Katarzyna Fronc <katarzynaanna.fronc@gmail.com>
2022-12-13 16:30:14 +0100

    tips calculator functions were created

--- Exercise12/tip-calculator/script.js
+++ Exercise12/tip-calculator/script.js
@@ -1,24 +1,56 @@
-function calculateTip() {
-  let billAmt = document.getElementById("bill").value;
-  let tipPercent = document.getElementById("tipPercent").value;
-  let numOfPeople = document.getElementById("people").value;
+const billInput = document.querySelector(".inputBill");
+const peopleInput = document.querySelector(".inputPeople");
+const tipPerPerson = document.getElementById("resultAmount");
+const totalPerPerson = document.getElementById("resultTotal");
+const tips = document.querySelectorAll(".form--tip");
+const tipCustom = document.querySelector(".customTip");
 
-  let total = (bill * tipPercent) / numOfPeople;
-  total = total.toFixed(2);
+billInput.addEventListener("input", billInputFun);
+peopleInput.addEventListener("input", peopleInputFun);
+tips.forEach(function (val) {
+  val.addEventListener("click", handleClick);
+});
+tipCustom.addEventListener("input", tipInputFun);
 
-  let total = (bill * tipPercent) / numOfPeople + billAmt;
-  total = total.toFixed(2);
+billInput.value = "0.0";
+peopleInput.value = "1";
+tipPerPerson.innerHTML = "$" + (0.0).toFixed(2);
+totalPerPerson.innerHTML = "$" + (0.0).toFixed(2);
 
-  document.getElementById("resultAmount").innerText = total;
-  document.getElementById("resultTotal").innerText = total;
+let billValue = 0.0;
+let peopleValue = 1;
+let tipValue = 0.0;
 
-  let btn = document.getElementById("tipPercent");
-  btn.addEventListener("click", function (e) {
-    alert("rachunek!");
-    e.preventDefault();
+function billInputFun() {
+  billValue = parseFloat(billInput.value);
+  calculateTip();
+}
+
+function peopleInputFun() {
+  peopleValue = parseFloat(peopleInput.value);
+  calculateTip();
+}
+
+function tipInputFun() {
+  tipValue = parseFloat(tipCustom.value / 100);
+
+  calculateTip();
+}
+
+function handleClick(event) {
+  tips.forEach(function (val) {
+    if (event.target.innerHTML === val.innerHTML) {
+      tipValue = parseFloat(val.innerHTML) / 100;
+    }
   });
+  calculateTip();
+}
 
-  document.getElementById("tipPercent").onclick = function () {
-    calculateTip();
-  };
+function calculateTip() {
+  if (peopleValue >= 1) {
+    let tipAmount = (billValue * tipValue) / peopleValue;
+    let total = (billValue + tipAmount) / peopleValue;
+    tipPerPerson.innerHTML = "$" + tipAmount.toFixed(2);
+    totalPerPerson.innerHTML = "$" + total.toFixed(2);
+  }
 }
